7d2b53c212ad474ebb9d2612611dfc06b37ed586
bender <bender@14-dtiweb-XXX.(none)>
2011-05-12 16:37:00 -0300

    Qui Mai 12 16:37:00 BRT 2011

---
+++ mytemplates/media/js/login.js
@@ -0,0 +1,41 @@
+
+function limpar(obj){
+	document.getElementById(obj).value="";
+	}
+
+
+function validaCampo(idCampo, mensagem, idAviso){
+
+
+}
+
+
+
+
+$(document).ready(function(){
+
+$('#username').click(function (){limpar('username');});
+$('#password').click(function (){limpar('password');});
+
+
+
+
+});
+
+
+function validaCampo(idCampo, mensagem, idAviso){
+
+
+}
+
+
+
+
+$(document).ready(function(){
+
+
+x.click=function (){
+	limpar('username');
+}
+
+});
