47944d2ebe285d19883b262e5f90786a856f948c
bender <bender@14-dtiweb-XXX.(none)>
2011-05-13 17:19:47 -0300

    Sex Mai 13 17:19:47 BRT 2011

---
+++ mytemplates/media/js/base.js
@@ -0,0 +1,26 @@
+
+function limpar(obj){
+	document.getElementById(obj).value="";
+	}
+
+
+
+
+
+
+$(document).ready(function(){
+$('#data1').click(function (){limpar('data1');});
+$('#data2').click(function (){limpar('data2');});
+$('#solicitante').click(function (){limpar('solicitante');});
+$('#endRua').click(function (){limpar('endRua');});
+$('#endNum').click(function (){limpar('endNum');});
+
+/*....Mascaras...*/
+$('#dataOcorrencia').mask("99/99/9999");
+$('#dataFinal').mask("99/99/9999");
+
+
+});
+
+
+
